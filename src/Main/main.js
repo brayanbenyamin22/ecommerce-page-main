012616888da89d80721f050d6cc6e8edde9739c9
Brayan Navarro <m13nav86@gmail.com>
2022-06-18 02:05:53 -0500

    Logica del slide terminada faltan unos pequeños detalles con la animación

--- src/Main/main.js
+++ src/Main/main.js
@@ -1,31 +1,56 @@
 import React, { useState } from "react";
 import './main.css'
 import imagen1 from '../images/image-product-1.jpg';
-/* import imagen1 from '../images/image-product-1.jpg';
 import imagen2 from '../images/image-product-2.jpg';
 import imagen3 from '../images/image-product-3.jpg';
-import imagen4 from '../images/image-product-4.jpg'; */
+import imagen4 from '../images/image-product-4.jpg'; 
 
-/* const slideImage = () => {
-    const image = require(`../images/image-product-1.jpg`);
-    return image;
-} */
 function Main () {
-
+    /* === Manera Compleja */
+    /* const imagenes = [
+        {
+            image: imagen1,
+            id: 1,
+        },
+        {
+            image: imagen2,
+            id: 2,
+        },
+        {
+            image: imagen3,
+            id: 3,
+        },
+        {
+            image: imagen4,
+            id: 4,
+        },
+    ]; 
+    const imagenActual = imagenes.find( () => {
+        if(imagenes.id === slideImage){
+            return imagenes.image;
+        }
+    });  */
+    /* ====FORMA SENCILLA ==== Si funciono xD*/
+    const images = [
+        imagen1,
+        imagen2,
+        imagen3,
+        imagen4,
+    ];
     /*==== Logica del Slide ====*/
-    const [slideImage, setSlideImage] = React.useState(1);
+    const [slideImage, setSlideImage] = React.useState(0);
 
     const nextImage = () => {
-        if(slideImage === 4){
-            setSlideImage(1);
+        if(slideImage === 3){
+            setSlideImage(0);
         }
         else {
             setSlideImage(slideImage + 1);
         }
     }
     const prevImage = () => {
-        if(slideImage === 1){
-            setSlideImage(4);
+        if(slideImage === 0){
+            setSlideImage(3);
         }
         else {
             setSlideImage(slideImage - 1);
@@ -56,7 +81,7 @@ function Main () {
                             </div>
                         </div>
                         <div className="swiper-slide">
-                            <img src={imagen1} alt="imagenes"/>
+                            <img src={images[slideImage]} alt="imagenes"/>
                         </div>
                     </div>
                 </div>
