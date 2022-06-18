d366ef38759e032ba0296e9f7d0c63e9866e465b
Brayan Navarro <m13nav86@gmail.com>
2022-06-18 00:48:53 -0500

    Cree la logica del slide con States

--- src/Main/main.js
+++ src/Main/main.js
@@ -1,31 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import './main.css'
 import imagen1 from '../images/image-product-1.jpg';
-import imagen2 from '../images/image-product-1.jpg';
-import imagen3 from '../images/image-product-1.jpg';
-import imagen4 from '../images/image-product-1.jpg';
-const imagenes = [
-    imagen1,
-    imagen2,
-    imagen3,
-    imagen4,
-]
-const previousImg = () => {
-}
+/* import imagen1 from '../images/image-product-1.jpg';
+import imagen2 from '../images/image-product-2.jpg';
+import imagen3 from '../images/image-product-3.jpg';
+import imagen4 from '../images/image-product-4.jpg'; */
+
+/* const slideImage = () => {
+    const image = require(`../images/image-product-1.jpg`);
+    return image;
+} */
 function Main () {
+
+    /*==== Logica del Slide ====*/
+    const [slideImage, setSlideImage] = React.useState(1);
+
+    const nextImage = () => {
+        if(slideImage === 4){
+            setSlideImage(1);
+        }
+        else {
+            setSlideImage(slideImage + 1);
+        }
+    }
+    const prevImage = () => {
+        if(slideImage === 1){
+            setSlideImage(4);
+        }
+        else {
+            setSlideImage(slideImage - 1);
+        }
+    }
+    console.log(slideImage);
     return (
         <main className="main-container">
             <div className="slider">
                 <div className="swiper swiper-main">
                     <div className="swiper-wrapper">
+                        <div className="swiper-buttons__container">
+                            <div className="swiper-buttons">
+                                <div className="swiper-button-prev">
+                                    <img
+                                        onClick={prevImage} 
+                                        src={require("../images/icon-previous.svg").default} 
+                                        alt="next"
+                                    />
+                                </div>
+                                <div className="swiper-button-next">
+                                    <img 
+                                        onClick={nextImage}
+                                        src={require("../images/icon-next.svg").default} 
+                                        alt="next"
+                                    />
+                                </div>
+                            </div>
+                        </div>
                         <div className="swiper-slide">
                             <img src={imagen1} alt="imagenes"/>
-                        </div>
-                        <div className="swiper-button-next">
-                            <img src={require("../images/icon-next.svg").default} alt="next"/>
-                        </div>
-                        <div className="swiper-button-prev">
-                            <img src={require("../images/icon-previous.svg").default} alt="next"/>
                         </div>
                     </div>
                 </div>
